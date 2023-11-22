5db62818f328bf8a7e702609b74e4ea48b21b30d
Frank Shan <frankqs0304@hotmail.com>
2023-11-22 18:22:14 +0800

    add install.ieda.ide.bash

---
+++ install.ieda.ide.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:mmk2410/intellij-idea
+sudo apt update
+
+sudo apt-get install intellij-idea-community
+#sudo apt-get install intellij-idea-ultimate
