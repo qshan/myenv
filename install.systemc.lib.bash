9b3b527fa00ad012c301696bf1bf90806326d382
Frank Shan <frankqs0304@hotmail.com>
2023-12-25 23:58:53 +0800

    bash_shell_example

---
+++ install.systemc.lib.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+  sudo wget -O systemc-2.3.0a.tar.gz http://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.0a.tar.gz
+  tar -xzvf systemc-2.3.0a.tar.gz
+  cd systemc-2.3.0a
+  sudo mkdir -p /usr/local/systemc-2.3.0/
+  mkdir objdir
+  cd objdir
+  ../configure --prefix=/usr/local/systemc-2.3.0
+  make
+  sudo make install
