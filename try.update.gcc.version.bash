172d566f6476911c966dcd9b7b45749d151c2b75
Frank Shan <frankqs0304@hotmail.com>
2024-01-14 01:56:52 +0800

    add try.update.gcc.version.bash

---
+++ try.update.gcc.version.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+
+export input_argument1="$1"
+echo "input_argument1 is ${input_argument1}"
+
+cd /usr/bin
+
+sudo ln -sf        cpp-${input_argument1} cpp
+sudo ln -sf        g++-${input_argument1} g++
+sudo ln -sf     gcc-ar-${input_argument1} gcc-ar
+sudo ln -sf     gcc-nm-${input_argument1} gcc-nm
+sudo ln -sf gcc-ranlib-${input_argument1} gcc-ranlib
+
+# #ls gcc g++ cpp gcc-nm gcc-ar gcc-ranlib -alh
+# sudo ln -sf        cpp-4.8 cpp
+# sudo ln -sf        g++-4.8 g++
+# sudo ln -sf     gcc-ar-4.8 gcc-ar
+# sudo ln -sf     gcc-nm-4.8 gcc-nm
+# sudo ln -sf gcc-ranlib-4.8 gcc-ranlib
+# #ls gcc g++ cpp gcc-nm gcc-ar gcc-ranlib -alh
+# #ls -alh ld
+
+# sudo ln -sf        cpp-9 cpp
+# sudo ln -sf        g++-9 g++
+# sudo ln -sf     gcc-ar-9 gcc-ar
+# sudo ln -sf     gcc-nm-9 gcc-nm
+# sudo ln -sf gcc-ranlib-9 gcc-ranlib
+
+ls gcc g++ cpp gcc-nm gcc-ar gcc-ranlib -alh
+
+cd -
