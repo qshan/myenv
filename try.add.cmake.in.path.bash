838929b388722fc9a95258eb5926f16b68a2b3c3
Frank Q Shan <frankqs0304@hotmail.com>
2024-10-27 14:26:39 +0800

    update

--- try.add.cmake.in.path.bash
+++ try.add.cmake.in.path.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 #export  PATH=~/data/tools/cmake/cmake-3.30.1-linux-x86_64/bin:$PATH
-export  PATH=~/data/tools/cmake/cmake-3.22.0-linux-x86_64/bin:$PATH
+export  PATH=~/data/tools/cmake/cmake-3.29.8-linux-x86_64/bin:$PATH
+#export  PATH=~/data/tools/cmake/cmake-3.22.0-linux-x86_64/bin:$PATH
