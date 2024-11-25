8df0275e125d11667982acd78a4f19a52709c67a
Frank Q Shan <frankqs0304@hotmail.com>
2024-11-25 09:30:54 +0800

    add info

--- try.add.chrome.repo.bash
+++ try.add.chrome.repo.bash
@@ -1,5 +1,10 @@
 #!/usr/bash
 
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+# sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
 sudo add-apt-repository --yes "deb http://dl.google.com/linux/chrome/deb/ stable main"
 sudo add-apt-repository --yes "deb http://dl.google.com/linux/chrome-remote-desktop/deb/ stable main"
 sudo apt update
