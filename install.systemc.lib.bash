6d47e70b7ac0fcea76f9271a12d325bbb5a84270
Frank Q Shan <frankqs0304@hotmail.com>
2025-01-06 14:45:48 +0800

    add info

--- install.systemc.lib.bash
+++ install.systemc.lib.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# https://www.accellera.org/downloads/standards/systemc
+
   sudo wget -O systemc-2.3.0a.tar.gz http://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.0a.tar.gz
   tar -xzvf systemc-2.3.0a.tar.gz
   cd systemc-2.3.0a
